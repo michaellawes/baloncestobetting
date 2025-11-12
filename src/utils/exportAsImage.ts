f5f1f6fb66d341c4ddca17872a53390ec4ee4927
michaellawes <mjalawes@gmail.com>
2025-11-11 22:31:42 -0800

    Add exportAsImage

---
+++ src/utils/exportAsImage.ts
@@ -0,0 +1,24 @@
+import html2canvas from "html2canvas-pro";
+
+const exportAsImage = async (
+  element: HTMLDivElement,
+  imageFileName: string,
+) => {
+  const canvas = await html2canvas(element);
+  const image = canvas.toDataURL("image/png", 1.0);
+  downloadImage(image, imageFileName);
+};
+
+export const downloadImage = async (blob: string, imageFileName: string) => {
+  const fakeLink = window.document.createElement("a");
+  fakeLink.style.display = "none";
+  fakeLink.download = imageFileName;
+
+  fakeLink.href = blob;
+  document.body.appendChild(fakeLink);
+  fakeLink.click();
+  document.body.removeChild(fakeLink);
+  fakeLink.remove();
+};
+
+export default exportAsImage;
