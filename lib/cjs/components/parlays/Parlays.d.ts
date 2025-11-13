b08c725c8d500463794b5871fde60a0ab2421bb4
michaellawes <mjalawes@gmail.com>
2025-11-13 12:04:40 -0800

    Fix ts

--- lib/cjs/components/parlays/Parlays.d.ts
+++ lib/cjs/components/parlays/Parlays.d.ts
@@ -4,6 +4,5 @@ export interface ParlaysViewerProps {
     setBalance: React.Dispatch<React.SetStateAction<number>>;
     user: UserData;
     setParlayFieldUpdate: React.Dispatch<React.SetStateAction<ParlayFieldUpdate>>;
-    setErrorMessage: React.Dispatch<React.SetStateAction<string>>;
 }
 export declare function Parlays(props: ParlaysViewerProps): import("react/jsx-runtime").JSX.Element;
