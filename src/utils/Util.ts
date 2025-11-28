9d92c7e7f425e707ca86dc52521f4a3e22510c35
michaellawes <mjalawes@gmail.com>
2025-11-28 02:29:59 -0800

    Update logic

--- src/utils/Util.ts
+++ src/utils/Util.ts
@@ -8,7 +8,7 @@ import {
   SqlPropSlate,
   SqlTeamMetadata,
   SupabaseParlay,
-  Team,
+  Team
 } from "./Interfaces";
 import supabase from "../config/supabaseConfig";
 import html2canvas from "html2canvas-pro";
@@ -710,11 +710,10 @@ export const getDailySlate = async (matchupId: number) => {
       matchup.home.live_score > 0
         ? matchup.home.live_score
         : parseFloat(matchup.home.team_total.text);
-    if (Math.abs(homeScore - roadScore) < 50) {
+    if (Math.abs(homeScore - roadScore) < 51) {
       matchup.isClose = true;
     }
   }
-  console.log(slate);
   return slate;
 };
 
