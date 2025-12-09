70ef2636670d6bb299f1aff6639a2f8c77f118f8
michaellawes <mjalawes@gmail.com>
2025-12-09 09:47:41 -0800

    Update matchup styling

--- src/components/matchup/Matchup.tsx
+++ src/components/matchup/Matchup.tsx
@@ -1,9 +1,19 @@
 import * as React from "react";
 import { useContext, useEffect } from "react";
-import { getPlayerNameIsTooLong, getStandardTime, getTeamNameIsTooLong, roundToInteger } from "../../utils/Util";
+import {
+  getPlayerNameIsTooLong,
+  getStandardTime,
+  getTeamNameIsTooLong,
+  roundToInteger,
+} from "../../utils/Util";
 import { TasksContext } from "../reducer/TasksContext";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBasketball, faFaceDizzy, faFaceGrimace, faFaceGrin } from "@fortawesome/free-solid-svg-icons";
+import {
+  faBasketball,
+  faFaceDizzy,
+  faFaceGrimace,
+  faFaceGrin,
+} from "@fortawesome/free-solid-svg-icons";
 import { IconProp } from "@fortawesome/fontawesome-svg-core";
 import { PropLine } from "../dashboard/wagers/PropLine";
 import { ErrorLander } from "../dashboard/ErrorLander";
